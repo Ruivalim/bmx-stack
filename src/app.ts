4d5bf7273371e68b99081ad240ac523b766f5dc5
Rui Valim <r.valim.junior@gmail.com>
2024-11-28 16:07:10 -0300

    Missing packages added

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,20 @@ import express, { Application } from 'express';
 import dotenv from 'dotenv';
 import fs from 'fs';
 import path from 'path';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
 dotenv.config();
 
 const app: Application = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
 
 app.use(express.static(path.join(__dirname, '../public')));
 
